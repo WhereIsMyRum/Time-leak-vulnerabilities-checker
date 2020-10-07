087efd148c84880e2a87dbc59a9f5c8620a9df22
WhereIsMyRum <polcik.piotr@gmail.com>
2020-10-07 17:26:52 +0200

    Updated create_nets script

--- create_nets_files.js
+++ create_nets_files.js
@@ -1,6 +1,7 @@
 const fs = require('fs');
 const nets = require('./visualisation/libraries/example-nets').nets
 
+fs.mkdirSync('time-leak-detector/nets', {recursive : true});
 Object.entries(nets).forEach(([key, value]) => {
     const data = JSON.stringify(value, null, 4);
     fs.writeFile(`./time-leak-detector/nets/${key}.json`, data, err => {
