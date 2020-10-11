96e08186538424cc9a25a17184f88439673a8fcb
WhereIsMyRum <polcik.piotr@gmail.com>
2020-10-11 13:30:38 +0200

    Added procfile

--- server.js
+++ server.js
@@ -1,9 +1,12 @@
 const express = require('express');
 const process = require('child_process');
-const app = express();
 const bodyParser = require('body-parser');
 const fs = require('fs');
-const PORT = process.env.PORT || 5000;
+
+const PORT = process.env.PORT || 8080;
+
+const app = express();
+
 
 
 
