2f7f29c620255637f342fab03c9ce679c61ffdd3
WhereIsMyRum <polcik.piotr@gmail.com>
2020-10-11 14:26:06 +0200

    .

--- server.js
+++ server.js
@@ -7,10 +7,6 @@ const PORT = process.env.PORT || 5000;
 
 const app = express();
 
-
-
-
-
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded());
 app.use(express.static(__dirname + '/visualisation'));
@@ -18,7 +14,7 @@ app.use(express.static(__dirname + '/visualisation'));
 app.post("/analyze", (request, response) => {
     //console.log(request.body.custom);
     //`${__dirname}/time-leak-detector/nets/net1.json`
-    let results = childProcess.execFileSync(`${__dirname}/time-leak-detector/main.exe`, [JSON.stringify(request.body.custom)]).toString().split('\r\n');
+    let results = childProcess.execFileSync(`${__dirname}/time-leak-detector/main`, [JSON.stringify(request.body.custom)]).toString().split('\r\n');
     const resultString = JSON.parse(JSON.stringify(request.body.custom));
     resultString.colors = {};
     results.forEach(result => {
