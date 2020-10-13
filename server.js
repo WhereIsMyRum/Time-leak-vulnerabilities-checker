c7f7930b38e180bc36416a49d0fc955a8050b058
WhereIsMyRum <polcik.piotr@gmail.com>
2020-10-13 17:18:01 +0200

    Fixed missing app var

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const childProcess = require('child_process');
+const app = express();
 const bodyParser = require('body-parser');
 const fs = require('fs');
 const PORT = 5000;
