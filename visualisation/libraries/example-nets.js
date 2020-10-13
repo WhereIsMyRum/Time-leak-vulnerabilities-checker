7e944fbeca17014ab26419f63d04b7432d00f340
WhereIsMyRum <polcik.piotr@gmail.com>
2020-10-13 16:42:27 +0200

    Modified nets

--- visualisation/libraries/example-nets.js
+++ visualisation/libraries/example-nets.js
@@ -217,7 +217,7 @@ const nets = {
             }
         }
     },
-    "12\.Combined-Multiple-Layers": {
+    "11\.Combined-Multiple-Layers": {
         "places": ["p1","p2","p3","p4","p5","p6","p7","p8","p9","p10","p11"],
         "transitions": {
             "high": ["H1","H2","H3","H4"],
