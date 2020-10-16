4956c613cd8fc2073f66a77510e529c1ce23b445
WhereIsMyRum <polcik.piotr@gmail.com>
2020-10-16 12:53:52 +0200

    Removed massives from predefined nets

--- create_nets_files.js
+++ create_nets_files.js
@@ -2,13 +2,15 @@ const fs = require('fs');
 const nets = require('./visualisation/libraries/example-nets').nets
 
 if (!fs.existsSync('time-leak-detector/nets')) {
-    fs.mkdirSync('time-leak-detector/nets', {recursive : true});
+    fs.mkdirSync('time-leak-detector/nets', { recursive: true });
 }
 Object.entries(nets).forEach(([key, value]) => {
-    const data = JSON.stringify(value, undefined, 4);
-    fs.writeFile(`./time-leak-detector/nets/${key}.json`, data, err => {
-        if (err) {
-            throw err;
-        }
-    });
+    if (value.results.length > 0) {
+        const data = JSON.stringify(value, undefined, 4);
+        fs.writeFile(`./time-leak-detector/nets/${key}.json`, data, err => {
+            if (err) {
+                throw err;
+            }
+        });
+    }
 });
