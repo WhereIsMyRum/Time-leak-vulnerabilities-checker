7855008422c016ba795066d3f7f7a199099517b8
WhereIsMyRum <polcik.piotr@gmail.com>
2020-10-16 14:08:26 +0200

    Created initial netPrunner

---
+++ time-leak-detector/include/time_leak/netPruner.hpp
@@ -0,0 +1,15 @@
+#ifndef NETPRUNER
+#define NETPRUNER
+
+#include "net.hpp"
+
+namespace time_leak
+{
+    class NetPruner
+    {
+        public:
+        Net PruneNet(Net net);
+    };
+}
+
+#endif
---
+++ time-leak-detector/src/time_leak/netPruner.cpp
@@ -0,0 +1,8 @@
+#include "../../include/time_leak/netPruner.hpp"
+
+using namespace std;
+
+time_leak::Net time_leak::NetPruner::PruneNet(time_leak::Net net)
+{
+    
+}
