65a5fb58243458756676aac7a64d6a4f744fad0f
Wiking <p.polcik1@gmail.com>
2020-12-04 13:08:56 +0100

    Updated graphs

--- utils/benchmarking.py
+++ utils/benchmarking.py
@@ -44,34 +44,48 @@ def plotResults(results, maxNoOfElements):
     line = numpy.linspace(0, maxNoOfElements, int(maxNoOfElements / step))
 
     creation = numpy.poly1d(numpy.polyfit(creationDuration[0], creationDuration[1], 2))
-    nPnC = numpy.poly1d(numpy.polyfit(noPruningNoConditional[0], noPruningNoConditional[1], 1))
-    PnC = numpy.poly1d(numpy.polyfit(pruningNoConditional[0], pruningNoConditional[1], 1))
-    nPC = numpy.poly1d(numpy.polyfit(noPruningConditional[0], noPruningConditional[1], 1))
+    nPnC = numpy.poly1d(numpy.polyfit(noPruningNoConditional[0], noPruningNoConditional[1], 2))
+    PnC = numpy.poly1d(numpy.polyfit(pruningNoConditional[0], pruningNoConditional[1], 2))
+    nPC = numpy.poly1d(numpy.polyfit(noPruningConditional[0], noPruningConditional[1], 2))
 
     plt.figure(1)
     plt.title('Creation duration')
     plt.xlabel('Number of elements')
     plt.ylabel('time [micro s]')
+
     plt.scatter(creationDuration[0], creationDuration[1], s=1, label='Creation scatter')
     plt.plot(line, creation(line), '-b', label="Creation duration")
     plt.legend(loc='best')
 
 
     plt.figure(2)
-    plt.title('Algorithm duration')
+    plt.title('Pruning Comparison')
     plt.xlabel('Number of Elements')
     plt.ylabel('time [micro s]')
 
+    plt.scatter(noPruningNoConditional[0], noPruningNoConditional[1], c='b', label='No prunning no conditional scatter',  s=1)
     plt.plot(line, nPnC(line), '-b', label="No pruning no conditional")
+    plt.scatter(pruningNoConditional[0], pruningNoConditional[1], c='r', s=1, label='Prunning no conditional scatter')
     plt.plot(line, PnC(line), '-r', label="Pruning no conditional")
+    plt.legend(loc='best')
+
+    plt.figure(3)
+    plt.title('Conditional comparison')
+    plt.xlabel('Number of Elements')
+    plt.ylabel('time [micro s]')
+
+    plt.scatter(noPruningNoConditional[0], noPruningNoConditional[1], c='b', s=1, label='No prunning no conditional scatter')
+    plt.plot(line, nPnC(line), '-b', label="No pruning no conditional")
+    plt.scatter(noPruningConditional[0], noPruningConditional[1], c='g', label='No prunning conditional scatter', s=1)
     plt.plot(line, nPC(line), '-g', label="No pruning conditional")
     plt.legend(loc='best')
+
 
 
     plt.show()
 
 def main():
-    maxNoOfElements = 1000
+    maxNoOfElements = 8000
 
     results = runBenchmark(maxNoOfElements)
     plotResults(results, maxNoOfElements)
